40ec61e3a6d0d48121bda789a88ca29475512410
SAVIO <savionaguicha@hotmail.com>
2025-03-09 18:45:07 -0500

    Tarea semana 12

---
+++ SEMANA 11/SEMANA 11 Ejercicio 1 Busqueda.py
@@ -0,0 +1,13 @@
+ # crear una matriz pequeña de (3*3)
+matriz1 = [1,2,3]
+matriz2 = [4,5,6]
+matriz3 = [7,8,9]
+matriz_de_matriz = [matriz1, matriz2, matriz3]
+print(matriz_de_matriz)
+print(matriz_de_matriz[0])
+print(matriz_de_matriz[1])
+print(matriz_de_matriz[2])
+print(matriz_de_matriz[0][0])
+print(matriz_de_matriz[0][2])
+print(matriz_de_matriz[1][0])
+print(matriz_de_matriz[2][2])
---
+++ SEMANA 11/SEMANA 11 Ejercicio 2 Ordenación.py
@@ -0,0 +1,10 @@
+# lista pequeña de (3*3)
+matriz1 = [1,2,3]
+matriz2 = [4,5,6]
+matriz3 = [7,8,9]
+matriz_de_matriz = [matriz1, matriz2, matriz3]
+print(matriz_de_matriz)
+print(matriz_de_matriz[0][:2])
+print(matriz_de_matriz[0][:2])
+print(matriz_de_matriz[0][:1])
+print(matriz_de_matriz[0][2:])
