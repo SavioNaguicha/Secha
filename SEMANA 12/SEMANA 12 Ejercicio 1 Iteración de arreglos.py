1697e547a9a219a8722e48f2878a45f8f099dde2
SAVIO <savionaguicha@hotmail.com>
2025-03-09 18:26:56 -0500

    Tarea semana 12

---
+++ SEMANA 12/SEMANA 12 Ejercicio 1 Iteración de arreglos.py
@@ -0,0 +1,11 @@
+# crear una matriz de (3D) temperaturas diarias en varias ciudades
+lista1 = ["Cenca =", "Quito =", "Guayaquil =","Puyo =", "Macas =", ]
+lista2 = ["temp: 10°c","7°c","8°c","9°c","10°c"]
+lista3 = ["Lunes","Martes", "Miercoles", " Jueves", "Viernes"]
+for lista1, lista2, lista3 in zip(lista1, lista2, lista3):
+    print(lista1, lista2, lista3)
+
+
+
+
+
