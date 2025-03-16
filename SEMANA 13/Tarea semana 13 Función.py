1d12f8a5f67900aa4c32945d288377455aae1f63
SAVIO <savionaguicha@hotmail.com>
2025-03-16 18:07:20 -0500

    Tarea semana 13

---
+++ SEMANA 13/Tarea semana 13 Función.py
@@ -0,0 +1,21 @@
+# Calcular la tenpertaura promedio de una ciudad durante un periodo de tiempo
+
+ciudades_temperaturas = {}
+Puyo =  ["tem: 10°c", "temp: 6°c","Temp: 5°c"]
+Macas = ["temp: 15°c","temp: 20°c"," temp: 13c" ]
+Quito = ["temp: 14°c","temp: 16°c", "temp: 17°c"]
+Guayaquil = ["temp: 20°c","temp: 24°c","temp: 30°c"]
+Cuenca = ["Temp: 18°C", "Temp:27°C","temp: 26°c"]
+ciudades_temperaturas = [Puyo, Macas, Quito, Guayaquil, Cuenca]
+print(ciudades_temperaturas)
+
+
+
+
+
+
+
+
+
+
+
