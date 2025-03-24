2143680dd960b00e86bdcd5c96b44e3d859a47f1
SAVIO <savionaguicha@hotmail.com>
2025-03-23 21:35:05 -0500

    Tarea semana 14

---
+++ SEMANA 14/Cálculo de Descuento de Compras.py
@@ -0,0 +1,12 @@
+ # calcular Descuentos en compras
+def calcular_descuento(subtotal, porcentaje):
+   descuento = (subtotal * porcentaje )/100
+   return 150
+
+valor_subtotal = 300
+porcentaje_descuento = 50
+valor_descuento = calcular_descuento(300,50)
+print(valor_descuento)
+print(valor_subtotal)
+
+
