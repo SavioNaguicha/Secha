07ec0af8dc29d93524395758e1290c55db29678b
SAVIO <savionaguicha@hotmail.com>
2025-03-30 20:32:39 -0500

    Tarea semana 15

---
+++ SEMANA 15/Tarea semana 15 crear un diccionario.py
@@ -0,0 +1,9 @@
+# crear un dicionario
+dicc = {"Nombre": "Savio Naguicha",
+        "Edad": "33 años",
+        "Ciudad": "Quito",
+        "Profeción": "Seguridad",
+        "Telefono": "0939060857",
+        "Correo Electronico": "savio@hotmail.com"}
+for clave,valor in dicc.items():
+    print(f"{clave}:{valor}")
